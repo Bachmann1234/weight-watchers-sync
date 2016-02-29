b239548f605e87b467774bcd8cc1485ca0818baf
Matt Bachmann <bachmann.matt@gmail.com>
2016-02-28 21:59:29 -0500

    A working usecase! I can sync the current day now

--- wwsync/fitbit.py
+++ wwsync/fitbit.py
@@ -1,6 +1,6 @@
 import webbrowser
+from datetime import datetime
 
-import os
 import sys
 
 import base64
@@ -8,6 +8,12 @@ import requests
 
 FITBIT_AUTH_URL = 'https://api.fitbit.com/oauth2/token?code={code}&client_id={client_id}&grant_type=authorization_code'
 FITBIT_PERMISSION_SCREEN = 'https://fitbit.com/oauth2/authorize?response_type=code&client_id={client_id}&scope={scope}'
+FITBIT_EDIT_FOOD_LOG_URL = 'https://api.fitbit.com/1/user/-/foods/log.json'
+FITBIT_FOOD_UNIT_URL = 'https://api.fitbit.com/1/foods/units.json'
+FITBIT_GET_FOOD_URL = 'https://api.fitbit.com/1/user/-/foods/log/date/{:%Y-%m-%d}.json'
+FITBIT_DELETE_FOOD_URL = 'https://api.fitbit.com/1/user/-/foods/log/{log_id}.json'
+
+DEFAULT_UNIT = 'unit'
 
 
 def auth(code, client_id, secret):
@@ -51,20 +57,81 @@ def get_code(client_id):
     )
 
 
-def main():
-    client_id = os.environ['WW_FB_ID']
-    secret = os.environ['WW_FB_SECRET']
-    code = get_code(client_id)
-    auth_response = auth(code, client_id, secret)
-    print(auth_response)
-    # Test api (stub)
-    print(
-        requests.get(
-            'https://api.fitbit.com/1/user/{}/foods/log/goal.json'.format('27PLNC'),
-            headers={'Authorization': 'Bearer {}'.format(auth_response['access_token'])}
-        ).json()
+def make_food_log(
+        auth_header,
+        ww_food_entry,
+        unit_dict,
+        date=datetime.now()
+):
+    """
+    Converts a ww food entry and saves it to fitbit
+    Args:
+        auth_header:
+            Authorization header for fitbit
+        ww_food_entry:
+            A dict with nutrition_info and food_log that will be used to record
+        unit_dict:
+            Result of a previous call to get_units, dict
+        date:
+            Date of the food log
+    """
+    # 2 is morning snack and 4 is afternoon snack but I
+    # don't have ww equivalents for that
+    ww_to_fitbit_meal = {
+        'morning': 1,
+        'midday': 3,
+        'evening': 5,
+        'anytime': 7,
+    }
+    food_log = ww_food_entry['food_log']
+    nutrition_info = ww_food_entry['nutrition_info']
+    ww_unit = food_log['portionName'].replace('(s)', '')
+    response = requests.post(
+        FITBIT_EDIT_FOOD_LOG_URL,
+        data={
+            'foodName': food_log['name'],
+            'mealTypeId': ww_to_fitbit_meal[food_log['timeOfDay']],
+            'unitId': unit_dict.get(ww_unit, unit_dict[DEFAULT_UNIT]),
+            'amount': food_log['portionSize'],
+            'date': "{:%Y-%m-%d}".format(date),
+            'calories': int(nutrition_info['calories']),
+            'sugars': nutrition_info['sugar'],
+            'sodium': nutrition_info['sodium'],
+            'protein': nutrition_info['protein'],
+            'saturatedFat': nutrition_info['saturatedFat'],
+            'totalFat': nutrition_info['fat'],
+            'dietaryFiber': nutrition_info['fiber'],
+            'totalCarbohydrate': nutrition_info['carbs']
+        },
+        headers=auth_header
     )
+    response.raise_for_status()
 
 
-if __name__ == '__main__':
-    main()
+def get_units(auth_header):
+    """
+    Calls out to fitbit to get the most up to date units
+    Args:
+        auth_header:
+            Authorization info for fitbit
+    returns:
+        dict from unit name to unit id
+    """
+    response = requests.get(FITBIT_FOOD_UNIT_URL, headers=auth_header)
+    response.raise_for_status()
+    units = {}
+    for unit in response.json():
+        units[unit['name']] = unit['id']
+    return units
+
+
+def clear_food_logs(auth_header, date=datetime.now()):
+    response = requests.get(FITBIT_GET_FOOD_URL.format(date), headers=auth_header)
+    response.raise_for_status()
+    food_logs = response.json()
+    for log in food_logs['foods']:
+        delete_response = requests.delete(
+            FITBIT_DELETE_FOOD_URL.format(log_id=log['logId']),
+            headers=auth_header
+        )
+        delete_response.raise_for_status()
---
+++ wwsync/sync.py
@@ -0,0 +1,28 @@
+import os
+from wwsync.fitbit import get_code, auth, get_units, clear_food_logs, make_food_log
+from wwsync.weight_watchers import get_nutrition_info_for_day
+
+
+def main():
+    client_id = os.environ['WW_FB_ID']
+    secret = os.environ['WW_FB_SECRET']
+    nutritional_info_for_day = get_nutrition_info_for_day(
+        os.environ['WW_USER'],
+        os.environ['WW_PASSWD']
+    )
+    code = get_code(client_id)
+    auth_response = auth(code, client_id, secret)
+    auth_header = {'Authorization': 'Bearer {}'.format(auth_response['access_token'])}
+    units = get_units(auth_header)
+    clear_food_logs(auth_header)
+
+    for log in nutritional_info_for_day:
+        make_food_log(
+            auth_header,
+            log,
+            units
+        )
+
+
+if __name__ == '__main__':
+    main()
