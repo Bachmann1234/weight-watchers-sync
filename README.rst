3c418ba9ab8b4a3357530f390af563da46e812ed
Matt Bachmann <bachmann.matt@gmail.com>
2016-03-05 10:41:44 -0500

    Add the ability to pass in a date

--- README.rst
+++ README.rst
@@ -2,10 +2,26 @@
 Weight Watchers Sync
 ====================
 
+I am in no way affiliated with Weight Watchers. I just wanted
+my fitbit account to sync better with my weight watchers one.
+
 This app can be used to sync your food journal with Fitbit.
 
-Current version focuses on summing up your journal and creating one entry in fitbit.
+This app depends on a few things:
+    1. You have a paid weight watcher's etools account
+    2. You have created a fitbit api application
 
-Future versions may sync each item individually.
+The sync app expects 4 environment variables defined
+    WW_FB_ID - Your fitbit app client id
+    WW_FB_SECRET - Your fitbit app client secret
+    WW_USER - Your Weight Watchers Username
+    WW_PASSWD - Your Weight Watchers Password
 
-This is a WIP project
+With those defined you can run sync. It will first scrape weight watchers to
+grab all your food for the current day. Then all fitbit food logs for the current
+date will be cleared and replaced with weight watcher food logs
+
+Optionally you can provide a date in the following format
+'%Y-%m-%d' (Example: 2016-03-5)
+
+If you do that the script will work with the provided date
--- wwsync/sync.py
+++ wwsync/sync.py
@@ -1,29 +1,38 @@
+import datetime
 import logging
 import pprint
 
+import sys
+
 import os
-from wwsync.fitbit import get_code, auth, get_units, clear_food_logs, make_food_log, get_food_logs
+from wwsync.fitbit import auth, get_units, clear_food_logs, make_food_log, get_food_logs
 from wwsync.weight_watchers import get_nutrition_info_for_day
 
 
 def main():
+    if len(sys.argv) > 1:
+        date = datetime.datetime.strptime(sys.argv[1], '%Y-%m-%d')
+    else:
+        date = datetime.datetime.now()
     logging.basicConfig(level='INFO')
     client_id = os.environ['WW_FB_ID']
     secret = os.environ['WW_FB_SECRET']
     nutritional_info_for_day = get_nutrition_info_for_day(
         os.environ['WW_USER'],
-        os.environ['WW_PASSWD']
+        os.environ['WW_PASSWD'],
+        date=date
     )
     auth_response = auth(client_id, secret)
     auth_header = {'Authorization': 'Bearer {}'.format(auth_response['access_token'])}
     units = get_units(auth_header)
-    clear_food_logs(auth_header, get_food_logs(auth_header))
+    clear_food_logs(auth_header, get_food_logs(auth_header, date=date))
 
     for log in nutritional_info_for_day:
         make_food_log(
             auth_header,
             log,
-            units
+            units,
+            date=date
         )
     # print final result
     pprint.pprint(get_food_logs(auth_header))
